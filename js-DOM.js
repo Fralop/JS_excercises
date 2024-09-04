10700a549e10b1b87be5cb1d2be77f366569a0b3
Fralop <117324827+Fralop@users.noreply.github.com>
2024-09-03 20:49:59 -0400

    Added red color

--- js-DOM.js
+++ js-DOM.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("body");
 const contentOne = document.createElement("p");
+contentOne.style.color = "red"
 contentOne.textContent = "Hey I'm red";
 container.appendChild(contentOne);
