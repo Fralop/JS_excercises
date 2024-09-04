d275e5d12cee295f00c748e7922fb14e6731b5ec
Fralop <117324827+Fralop@users.noreply.github.com>
2024-09-03 20:59:37 -0400

    Added the blue h3

--- js-DOM.js
+++ js-DOM.js
@@ -3,3 +3,8 @@ const contentOne = document.createElement("p");
 contentOne.style.color = "red"
 contentOne.textContent = "Hey I'm red";
 container.appendChild(contentOne);
+
+const contentTwo = document.createElement("h3");
+contentTwo.style.color = "blue";
+contentTwo.textContent = "I'm a blue h3!"
+container.appendChild(contentTwo);
