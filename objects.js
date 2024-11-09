0760e31706825c3121c924d3a02c83f9f2d069cb
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-09 13:09:14 -0500

    Finished excercise adding console.log for visualization

--- objects.js
+++ objects.js
@@ -15,12 +15,22 @@ function addAll(sum, num) {
 
 function SumOfTripledEvens (array) {
     let filteredArr = array.filter(isEven);
+    console.log(array);
+    console.log(filteredArr);
     let mappedArr = filteredArr.map(byThree);
+    console.log(mappedArr);
     let reducedArr = mappedArr.reduce(addAll,0);
     return reducedArr;
 }
+// Define array
+let arr = [2, 3, 8, 10, 13, 15, 17, 20, 21, 23, 25, 26]
 
 //show in console
-
+console.log(SumOfTripledEvens(arr));
 
 //show in html
+const body = document.querySelector('body');
+const para = document.createElement('p');
+para.textContent = SumOfTripledEvens(arr);
+body.appendChild(para);
+console.log(para);
